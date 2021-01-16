e9bceb76580674fe35f48577e1956a2d4893c9be
GilZm <gilzm1.0@gmail.com>
2021-01-16 16:05:44 +0100

    Update entrypoint.sh

--- nfsen-ng/entrypoint.sh
+++ nfsen-ng/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sources=`cat /tmp/sources.conf | sed -re "s/^([^:]*):.*$/'\1',/" | tr '\n' ' ' `
+sources=`cat /tmp/sources.conf | sed -re "s/^([^;]*);.*$/'\1',/" | tr '\n' ' ' `
 sed -e "s/'router',/$sources/" /var/www/html/backend/settings/settings.tmpl > /var/www/html/backend/settings/settings.php
 
 if php -f backend/settings/settings.php; then
