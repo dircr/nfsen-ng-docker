b6cb1531e6d39fe65b36ae6fb71df24fbbb1e938
GilZm <gilzm1.0@gmail.com>
2021-01-16 16:03:44 +0100

    Update entrypoint.sh
    multiple source update

--- nfsen-ng/entrypoint.sh
+++ nfsen-ng/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sources=`cat /tmp/sources.conf | sed -re "s/^([^:]*):.*$/'\1',/" | tr '\n' ' ' `
+sed -e "s/'router',/$sources/" /var/www/html/backend/settings/settings.tmpl > /var/www/html/backend/settings/settings.php
 
-/var/www/html/backend/cli.php start
-apache2-foreground
+if php -f backend/settings/settings.php; then
+    /var/www/html/backend/cli.php start
+    apache2-foreground
+fi
